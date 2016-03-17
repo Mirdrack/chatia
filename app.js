b75d60e6095f040e2e0bf986494dfaa4e898fbea
Clemente Estrada <mirdrack@gmail.com>
2016-03-17 15:15:32 -0600

    Added start script

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var flash = require('connect-flash-light');
+var flash = require('connect-flash');
 var passport = require('passport');
 var app = express();
 
